a70d0070f5775f8f11cc96c8e3337097434477f3
A. AJINOU <149362486+Nano-a@users.noreply.github.com>
2026-02-14 01:28:01 +0100

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Portfolio-A-AJINOU/" : "/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
